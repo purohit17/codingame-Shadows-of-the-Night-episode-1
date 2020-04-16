390377bc877245f8b40fa9d709cd2461d12d6b06
purohit17 <53011118+purohit17@users.noreply.github.com>
2020-04-16 19:52:35 +0530

    Update gamecode.java

--- gamecode.java
+++ gamecode.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-/**
+/** PARTH PUROHIT
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
@@ -13,77 +13,78 @@ class Player {
         int W = in.nextInt(); // width of the building.
         int H = in.nextInt(); // height of the building.
         int N = in.nextInt(); // maximum number of turns before game over.
+        
         int X0 = in.nextInt();
         int Y0 = in.nextInt();
 
-        int x = X0; // x is eaier than X0
-        int y = Y0; //y is easier than Y0
+       float x = X0; // converted to float for proper positon while calculating
+        float y = Y0; 
         
-        int xMin = -1;
-        int xMax = W;
-        int yMax = H;
-        int yMin = -1;
+        float xMin = 0;
+        float xMax = W;
+        float yMax = H;
+        float yMin = 0;
         // game loop
         while (true) {
             String bombDir = in.next(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
 
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
-
-            if(bombDir.equals("U")){
+ if(bombDir.equals("U")){
                 yMax = y;
-                System.out.println(x + " " + (y-((yMax-yMin)/2)));
+                System.out.println((int)x + " " + (int)(y-((yMax-yMin)/2)));
                 y = (y-((yMax-yMin)/2));
             }
             else if(bombDir.equals("D")){
                 yMin = y;
-                System.out.println(x + " " + (y+((yMax-yMin)/2)));
+                System.out.println((int)x + " " + (int)(y+((yMax-yMin)/2)));
                 y = (y+((yMax-yMin)/2));
                 
             }
             else if(bombDir.equals("L")){
                 xMax = x;
-                System.out.println((x-((xMax-xMin)/2)) + " " + y);
+                System.out.println((int)(x-((xMax-xMin)/2)) + " " + (int)y);
                 x = (x-((xMax-xMin)/2));
             }
             else if(bombDir.equals("R")){
                 xMin = x;
-                System.out.println((x+((xMax-xMin)/2)) + " " + y);
+                System.out.println((int)(x+((xMax-xMin)/2)) + " " + (int)y);
                 x = (x+((xMax-xMin)/2));
             }
             else if(bombDir.equals("UR")){
                 xMin = x;
                 yMax = y;
-                System.out.println((x+((xMax-xMin)/2)) + " " + (y-((yMax-yMin)/2)));
+                System.out.println((int)(x+((xMax-xMin)/2)) + " " + (int)(y-((yMax-yMin)/2)));
                 x = (x+((xMax-xMin)/2));
                 y = (y-((yMax-yMin)/2));
             }
             else if(bombDir.equals("UL")){
                 xMax = x;
                 yMax = y;
-                System.out.println((x-((xMax-xMin)/2)) + " " + (y-((yMax-yMin)/2)));
+                System.out.println((int)(x-((xMax-xMin)/2)) + " " + (int)(y-((yMax-yMin)/2)));
                 x = (x-((xMax-xMin)/2));
                 y = (y-((yMax-yMin)/2));
             }
             else if(bombDir.equals("DR")){
                 xMin = x;
                 yMin = y;
-                System.out.println((x+((xMax-xMin)/2)) + " " + (y+((yMax-yMin)/2)));
+                System.out.println((int)(x+((xMax-xMin)/2)) + " " +(int) (y+((yMax-yMin)/2)));
                 x = (x+((xMax-xMin)/2));
                 y = (y+((yMax-yMin)/2));
             }
             else if(bombDir.equals("DL")){
                 xMax = x;
                 yMin = y;
-                System.out.println((x-((xMax-xMin)/2)) + " " + (y+((yMax-yMin)/2)));
+                System.out.println((int)(x-((xMax-xMin)/2)) + " " + (int)(y+((yMax-yMin)/2)));
                 x = (x-((xMax-xMin)/2));
                 y = (y+((yMax-yMin)/2));
             }
             
+           
         
 
             // the location of the next window Batman should jump to.
-            System.out.println("0 0");
+           // System.out.println("0 0");
         }
     }
 }
